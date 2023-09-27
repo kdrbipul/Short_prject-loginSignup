bd89c336c820779d2be01d92969877c6c352d130
kdrbipul <kdrbipul@gmail.com>
2023-09-27 16:42:08 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const showPopupBtn = document.querySelector('.login-btn');
+const hidePopupBtn = document.querySelector('.form-popup .close-btn');
+const formPopup = document.querySelector(".form-popup")
+const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a");
+console.log(loginSignupLink);
+
+
+
+showPopupBtn.addEventListener("click",()=>{
+    document.body.classList.toggle("show-popup");
+});
+
+
+hidePopupBtn.addEventListener("click", ()=>showPopupBtn.click());
+
+
+
+loginSignupLink.forEach(link => {
+    link.addEventListener("click", (e) =>{
+        e.preventDefault();
+        // console.log(link.id);
+        formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup");
+    })
+})
