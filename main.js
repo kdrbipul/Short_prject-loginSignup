b438f582638af57df7c09a6ea15f93ba578fc8aa
kdrbipul <kdrbipul@gmail.com>
2023-09-27 21:28:54 +0600

    code kaj kore na

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const showPopupBtn = document.querySelector('.login-btn');
 const hidePopupBtn = document.querySelector('.form-popup .close-btn');
+const menuBtn = document.querySelector('.navbar .logo .menu-btn');
+console.log(menuBtn);
+const navbarMenu = document.querySelector(".navbar .links");
 const formPopup = document.querySelector(".form-popup")
 const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a");
 console.log(loginSignupLink);
+
+
+menuBtn.addEventListener("click", () => {
+    navbarMenu.classList.toggle("show-menu");
+});
+
 
 
 
